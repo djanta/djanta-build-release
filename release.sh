0aa2e2662f02316f85004b8095ccfb870ebf1a9f
stanislaska <koffi.ass@gmail.com>
2019-11-17 18:57:39 -0600

    000000: Introducing maven deploy function

--- release.sh
+++ release.sh
@@ -27,14 +27,8 @@ esac
 # shellcheck disable=SC1090
 source "${basedir}"/common.sh
 
-if [ "$#" -eq 0 ] &&  [ ! -f ".variables" ]; then
+if [[ "$#" -eq 0 ]] &&  [[ ! -f ".variables" ]]; then
   error_exit "Insuffisant command argument"
-fi
-
-# Load the project given .variables file if any
-if [ -f ".variables" ]; then
-  colored --blue "Exporting .variables file ..."
-  export_properties .variables
 fi
 
 # Load the pom version
@@ -59,14 +53,14 @@ safe_checkout() {
   git fetch origin "${branch}":origin/"${branch}"
   commit_local="$(git show --pretty='format:%H' "${branch}")"
   commit_remote="$(git show --pretty='format:%H' origin/"${branch}")"
-  if [ "$commit_local" != "$commit_remote" ]; then
+  if [[ "$commit_local" != "$commit_remote" ]]; then
     echo "${branch} on remote 'origin' has commits since the version under release, aborting"
     exit 1
   fi
 }
 
-update_nextrelease() {
-  if [ -f .variables ]; then
+update_release__() {
+  if [[ -f .variables ]]; then
     colored --blue "Updating next release version in (.variables) file ..."
     sed -i "s/NEXT_RELEASE=${1}/NEXT_RELEASE=${2}/g" .variables
   fi
@@ -76,11 +70,12 @@ update_nextrelease() {
 # Deploy the given profiles
 # shellcheck disable=SC2116
 ##
-profile_deploy() {
+__mvn_deploy__() {
   IFS=';' # hyphen (;) is set as delimiter
   read -ra ADDR <<< "${MVN_PROFILES:-}" # str is read into an array as tokens separated by IFS
   for i in "${ADDR[@]}"; do # access each element of array
     ./mvnw [ -n "${MVN_BASHMODE}" ] && $(echo "${MVN_BASHMODE}") [ -n "${MVN_DEBUG}" ] && $(echo "${MVN_DEBUG}") \
+      [ -n "${MVN_VARG}" ] && $(echo "${MVN_VARG}") \
       --no-snapshot-updates -P"$i" -DskipTests=true deploy
   done
   IFS=' ' # reset to default value after usage
@@ -97,7 +92,7 @@ profile_deploy() {
 #}
 
 # shellcheck disable=SC2154
-__release__() {
+release__() {
 
   argv inarg '--arg' "${@:1:$#}"
   argv intag '--tag' "${@:1:$#}"
@@ -138,23 +133,22 @@ __release__() {
   ./mvnw -B -X "${label}" -Dmvn.tag.prefix="${inlabel}-" scm:tag
 
   #Temporally fix to manually deploy (Deploy the new release tag)
-  profile_deploy "$inprofile" "${inarg}" # Deploy after version tag is created
+  __mvn_deploy__ "${inprofile}" "${inarg}" # Deploy after version tag is created
 
   # Update the versions to the next snapshot
   ./mvnw -B -X versions:set scm:checkin "${snapshot_argv}" -DgenerateBackupPoms=false \
     -Dmessage="[travis skip] updating versions to next development iteration ${snapshot}"
 
   #Temporally fix to manually deploy (Deploy the new snapshot)
-  profile_deploy "$inprofile" "${inarg}" # Deploy after snapshot version is created
+  __mvn_deploy__ "${inprofile}" "${inarg}" # Deploy after snapshot version is created
 }
 
 # Incremental versioning
 # shellcheck disable=SC2006
 # shellcheck disable=SC2154
+# shellcheck disable=SC2236
 api_version() {
-
-  # shellcheck disable=SC2236
-  if [ ! -z "$NEXT_RELEASE" ]; then
+  if [[ ! -z "$NEXT_RELEASE" ]]; then
     tag="$NEXT_RELEASE"
   else
     # extract the release version from the pom file
@@ -173,7 +167,7 @@ api_version() {
 
   [[ ! -z "$insnapshot" ]] && snapshot="$insnapshot" || snapshot=$(increment "${tag}")
 
-  __release__ --tag="${tag}" --tag-prefix="${inlabel:-release}" --snapshot="${snapshot}" --arg="${invarg:-}"
+  release__ --tag="${tag}" --tag-prefix="${inlabel:-release}" --snapshot="${snapshot}" --arg="${invarg:-}"
 }
 
 #Date based versioning
@@ -197,17 +191,17 @@ ts_version() {
   exists is_incremental '--continue-snapshot' "${@:1:$#}"
   argv innextsnapshot '--next-snapshot' "${@:1:$#}"
 
-  [ ! -z "$NEXT_RELEASE" ] && nextrelease="$NEXT_RELEASE" || nextrelease=$(date +'%y.%m.%d')
-  [ ! -z "$informat" ] && format="$informat" || format='%y.%m.%d'
-  [ ! -z "$fulldate" ] && now=$(date -j -f "${format}" "$fulldate" +"${format}") || now="$nextrelease"
-  [ ! -z "$inday" ] && d="$inday" || d="$(date -j -f "${format}" "$now" '+%d')"
-  [ ! -z "$inmonth" ] && m="$inmonth" || m="$(date -j -f "${format}" "$now" '+%m')"
-  [ ! -z "$inyear" ] && y="$inyear" || y="$(date -j -f "${format}" "$now" '+%y')"
+  [[ ! -z "$NEXT_RELEASE" ]] && nextrelease="$NEXT_RELEASE" || nextrelease=$(date +'%y.%m.%d')
+  [[ ! -z "$informat" ]] && format="$informat" || format='%y.%m.%d'
+  [[ ! -z "$fulldate" ]] && now=$(date -j -f "${format}" "$fulldate" +"${format}") || now="$nextrelease"
+  [[ ! -z "$inday" ]] && d="$inday" || d="$(date -j -f "${format}" "$now" '+%d')"
+  [[ ! -z "$inmonth" ]] && m="$inmonth" || m="$(date -j -f "${format}" "$now" '+%m')"
+  [[ ! -z "$inyear" ]] && y="$inyear" || y="$(date -j -f "${format}" "$now" '+%y')"
 
   local sep="${seperator:-.}"
   local ver="${y}${sep}${m}${sep}${d}"
 
-  [ ! -z "$inpatch" ] && tag="${ver}-${inpatch}" || tag="$ver"
+  [[ ! -z "$inpatch" ]] && tag="${ver}-${inpatch}" || tag="$ver"
 
   ##
   # IF '--continue-snapshot' was passed need to increment the snapshot version from the existing pom file
@@ -224,9 +218,9 @@ ts_version() {
 #  fi
 
   # shellcheck disable=SC2154
-  [ ! -z "${innextsnapshot}" ] && snapshot="${innextsnapshot}" #|| snapshot="${y}${sep}${m}${sep}$(($(date '+%d') + 1))-SNAPSHOT"
+  [[ ! -z "${innextsnapshot}" ]] && snapshot="${innextsnapshot}" #|| snapshot="${y}${sep}${m}${sep}$(($(date '+%d') + 1))-SNAPSHOT"
 
-  __release__ --tag="${tag}" --tag-prefix="${inlabel:-release}" --snapshot="${snapshot:-}" --arg="${invarg:-}"
+  release__ --tag="${tag}" --tag-prefix="${inlabel:-release}" --snapshot="${snapshot:-}" --arg="${invarg:-}"
 }
 
 help_message () {
@@ -237,6 +231,7 @@ help_message () {
   #./release timestamp [--format=.., --full-date=..[[--year.., --month=.., --day=..]], --patch]
 
   Global:
+
     --label The given expect label used to tag the released version (release|tag|rc), etc...
     --next-snapshot Define this option (no matter the value) to indicate the ongoing snapshot version
     --patch Use this option to define the current patching version stage
@@ -246,6 +241,7 @@ help_message () {
     --profile this option define your maven profile. ex: sonatype,other. If you wish to run your profile separaly, please use a comma (;) separator ex: sonatype,other;!sonatype,github
 
   Options:
+
   -h, --help [(timestamp|ts) | (increment|api) ] Display this help message within the given command and exit.
   --timestamp [timestamp | -- ts | ts]: Release the current project based on timestamp format (e.g: $(date +'%y.%m.%d'))
   --increment [increment | --api | api]: Release the current project, by continueing the current version.
@@ -284,20 +280,28 @@ if [[ $1 =~ ^((--)?((timestamp|ts)|(api|increment)|(help)))$ ]]; then
   INDEX=2
 else
   INDEX=1
-  [ ! -z "$RELEASE_STYLE" ] && XCMD="$RELEASE_STYLE" || XCMD='--help'
+  [[ ! -z "${RELEASE_STYLE}" ]] && XCMD="$RELEASE_STYLE" || XCMD='--help'
 fi
 
-exists indebug '--debug' "${@:$INDEX:$#}"
-argv insettingfile '--setting-file' "${@:$INDEX:$#}"
-exists inbashmodel '--bash-mode' "${@:$INDEX:$#}"
-argv inprofile '--profile' "${@:$INDEX:$#}"
-argv invarg '--varg' "${@:$INDEX:$#}"
+if [[ "${XCMD}" != "--help" ]] && [[ "${XCMD}" != "-h" ]]; then
+  exists indebug '--debug' "${@:$INDEX:$#}"
+  argv insettingfile '--setting-file' "${@:$INDEX:$#}"
+  exists inbashmodel '--bash-mode' "${@:$INDEX:$#}"
+  argv inprofile '--profile' "${@:$INDEX:$#}"
+  argv invarg '--varg' "${@:$INDEX:$#}"
 
-[ "${inbashmodel}" ] && $(export "MVN_BASHMODE"="-B") || colored --yellow "[Option] Maven bash mode Off"
-[ "${indebug}" ] && $(export "MVN_DEBUG"="-X") || colored --red "[Option] Maven debug Off"
-[ -n "${insettingfile}" ] && $(export "MVN_SETTINGS"="${insettingfile}") || colored --yellow "[Option] Maven settings Off"
-[ -n "${inprofile}" ] && $(export "MVN_PROFILES"="${inprofile}") || colored --yellow "[Option] Maven profiles Off"
-[ -n "${invarg}" ] && $(export "MVN_VARG"="${invarg}")
+  [[ "${inbashmodel}" ]] && $(export "MVN_BASHMODE"="-B") || colored --yellow "[Option] Maven bash mode Off"
+  [[ "${indebug}" ]] && $(export "MVN_DEBUG"="-X") || colored --red "[Option] Maven debug Off"
+  [[ -n "${insettingfile}" ]] && $(export "MVN_SETTINGS"="${insettingfile}") || colored --yellow "[Option] Maven settings Off"
+  [[ -n "${inprofile}" ]] && $(export "MVN_PROFILES"="${inprofile}") || colored --yellow "[Option] Maven profiles Off"
+  [[ -n "${invarg}" ]] && $(export "MVN_VARG"="${invarg}")
+
+  # Load the project given .variables file if any
+  if [[ -f ".variables" ]]; then
+    colored --blue "Exporting .variables file ..."
+    export_properties .variables
+  fi
+fi
 
 case ${XCMD} in
   -h|--help)
@@ -312,7 +316,7 @@ case ${XCMD} in
     ;;
 esac
 
-if [ -n "${XCMD}" ]; then
+if [[ -n "${XCMD}" ]]; then
   ${XCMD} "${@:$INDEX:$#}"
   graceful_exit ${?}
 else
