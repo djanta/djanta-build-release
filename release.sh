c693f0133fadc7ecfebc8a2c190f6b07942bca1e
stanislaska <koffi.ass@gmail.com>
2019-11-23 14:05:22 -0600

    000000: Debuging .variables release version replacement

--- release.sh
+++ release.sh
@@ -296,7 +296,7 @@ if [[ "${XCMD}" != "--help" ]] && [[ "${XCMD}" != "-h" ]]; then
   export RELEASE_BRANCH="${RELEASE_BRANCH:-release}"
 
   # Check if we start the tag release from from the expected branch.
-  [[ "${RELEASE_BRANCH}" != "$(git_branch)" ]] && error_exit "Expecting release should be" \"${RELEASE_BRANCH}\""
+  [[ "${RELEASE_BRANCH}" != "$(git_branch)" ]] && error_exit "Expecting release should be \"${RELEASE_BRANCH}\""
 fi
 
 case ${XCMD} in
