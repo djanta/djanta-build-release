4d32fc2deae0ebbe0acd5657323f6ca06eae87b1
stanislaska <koffi.ass@gmail.com>
2020-02-08 19:01:18 -0600

    prepare release 0.3.1

--- release.sh
+++ release.sh
@@ -80,7 +80,7 @@ deploy() {
   read -ra PROFILES <<< "${MVN_PROFILES:-}" # str is read into an array as tokens separated by IFS
   for profile in "${PROFILES[@]}"; do # access each element of array
     deploy_cmd="${MVN_SETTINGS:-} ${MVN_BASHMODE:-} ${MVN_DEBUG:-} ${MVN_VARG:-} -P$profile -DskipTests=true deploy"
-    colored --cyan "[deploy] - deploying with command: $deploy_cmd"
+    #colored --cyan "[deploy] - deploying with command: $deploy_cmd"
 #    $(./mvnw ${deploy_cmd} -DskipTests=true deploy)
     __run__ ${deploy_cmd} -DskipTests=true deploy
   done
